0000c2777799f8143770ae61f25704df520fda8c
voidedstarlight <haii@voided.systems>
2025-12-15 04:40:47 +0100

    feat: auth ui

--- apps/web/src/views/auth/view.ts
+++ apps/web/src/views/auth/view.ts
@@ -1,13 +1,29 @@
+import getContent from "../../layout";
 import getSession, { authGeneric } from "../../auth";
 
-function authView() {
-	void getSession().then(async data => {
-		if (data) {
-			document.location = "/dash?login=2";
-			return;
-		}
+function renderForm() {
+	void import("./view.css");
 
-		const auth_result = await authGeneric("a@a.com", "abcdef");
+	const content = getContent();
+	const form = document.createElement("div");
+	content.appendChild(form);
+	form.classList.add("form");
+
+	const email_input = document.createElement("input");
+	form.appendChild(email_input);
+	email_input.placeholder = "Email";
+
+	const password_input = document.createElement("input");
+	form.appendChild(password_input);
+	password_input.type = "password";
+	password_input.placeholder = "Password";
+
+	const confirm = document.createElement("button");
+	form.appendChild(confirm);
+	confirm.innerText = "Log In or Sign Up";
+
+	confirm.addEventListener("click", async () => {
+		const auth_result = await authGeneric(email_input.value, password_input.value);
 
 		if (auth_result === 0) {
 			document.location = "/dash?login=1";
@@ -16,6 +32,18 @@ function authView() {
 
 		console.error("[auth] failed to authenticate");
 		console.error(auth_result);
+
+	});
+}
+
+function authView() {
+	void getSession().then(data => {
+		if (data) {
+			document.location = "/dash?login=2";
+			return;
+		}
+
+		renderForm();
 	});
 }
 
